1db84063ddaf468d712aaf9accca7a78792af7e0
Alekandr-zero <zeroday9898@gmail.com>
2021-11-24 16:05:01 +0300

    Добавление статических файлов

--- README.md
+++ README.md
@@ -2,19 +2,20 @@
 
 1. **pip install -r requirements.txt**
 2. **go to directory "./blog"**
-3. **Start '_Python_':**
+3. **Start '_Python_'**
+4. **Create a database:**
 
 ```python
 >>> from app import db
 >>> db.create_all()
 ```
 
-4. **Create multiple articles:**
+5. **Create multiple articles:**
 
 ```python
 >>> from app import Post
->>> post_1 = Post(title = "Title post-1", text = "Text post-1")
->>> post_2 = Post(title = "Title post-2", text = "Text post-2")
+>>> post_1 = Post(title = "Title post 1", text = "Text post 1", pathImg = "./media/post-img-1.jpg")
+>>> post_2 = Post(title = "Title post 2", text = "Text post 2", pathImg = "./media/post-img-2.jpg")
 >>> db.session.add(post_1)
 >>> db.session.add(post_2)
 >>> db.session.commit()
--- blog/app.py
+++ blog/app.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask
 from flask import render_template
 from flask import url_for
@@ -5,7 +7,8 @@ from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_folder = "static")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -14,13 +17,15 @@ db = SQLAlchemy(app)
 class Post(db.Model):
 	"""docstring for Posts"""
 
-	id = db.Column(db.Integer, primary_key = True)
+	id = db.Column(db.Integer(), primary_key = True)
 	title = db.Column(db.String(100), nullable = False)
-	text = db.Column(db.Text, nullable = False)
+	text = db.Column(db.Text(), nullable = False)
+	pathImg = db.Column(db.String(), nullable = True)
+	pubDate = db.Column(db.DateTime(), nullable = False, default = datetime.now().date())
 
 
 	def __repr__(self):
-		return self.title
+		return f"{self.title} - id: {self.id}"
 
 
 @app.route("/index")
@@ -38,6 +43,17 @@ def index():
 @app.route("/about")
 def about():
 	return render_template("about.html")
+
+
+@app.route("/post-<int:id>")
+def postDetail(id):
+	post = Post.query.filter_by(id = id).first()
+
+	data = {
+		"post": post
+	}
+
+	return render_template("postDetail.html", data = data)
 
 
 if __name__ == '__main__':
